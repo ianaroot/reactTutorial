deeeafa5d93b00ba18ce124a85624c97d1ed8f8f
ianaroot <ianaroot@gmail.com>
2018-01-31 14:09:57 -0500

    currency list modal

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { FlatList, View, StatusBar } from 'react-native';
 import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
@@ -7,8 +8,12 @@ const TEMP_CURRENT_CURRENCY = 'CAD';
 
 
 class CurrencyList extends Component {
+  static propTypes = {
+    navigation: PropTypes.object,
+  }
   handlePress = () => {
     console.log('row press');
+    this.props.navigation.goBack(null);
   };
 
   render() {
